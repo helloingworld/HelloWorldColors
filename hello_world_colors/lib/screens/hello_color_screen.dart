157221476c946a73583a652f9c878e1201c8e9e4
devliber <dev@devliber.com>
2020-11-24 19:34:32 +0200

    Try various layouts for the Hello Color Screen info tab

--- hello_world_colors/lib/screens/hello_color_screen.dart
+++ hello_world_colors/lib/screens/hello_color_screen.dart
@@ -29,9 +29,20 @@ class HelloColorScreen extends StatelessWidget {
         backgroundColor: namedColor.color,
         appBar: AppBar(
           title: Text(namedColor.name),
+          actions: [
+            // IconButton(
+            //   icon: const Icon(Icons.copy),
+            //   onPressed: () {},
+            // ),
+            IconButton(
+              icon: const Icon(Icons.open_in_browser),
+              onPressed: () {},
+            ),
+          ],
           bottom: const TabBar(
             tabs: <Tab>[
               // Tab(text: 'Hello'),
+              // Tab(text: 'Info'),
               Tab(icon: Icon(Icons.language)),
               Tab(icon: Icon(Icons.info_outline)),
             ],
@@ -44,7 +55,7 @@ class HelloColorScreen extends StatelessWidget {
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 Text(
-                  'Hello, World! on',
+                  'Hello, World! I\'m',
                   style: Theme.of(context).textTheme.headline1.copyWith(
                         color: textColor,
                         fontSize: 34.0,
@@ -144,35 +155,71 @@ class _ColorInfoList extends StatelessWidget {
     final Color contrastColor = namedColor.isDark ? Colors.white : Colors.black;
     return ListTileTheme(
       textColor: contrastColor,
+      // iconColor: contrastColor.withOpacity(0.75),
       iconColor: contrastColor,
-      child: ListView(
-        // shrinkWrap: true,
-        children: <Widget>[
-          _ColorInfoListTile(info: namedColor.name),
-          _ColorInfoListTile(info: namedColor.toHexTriplet()),
-          _ColorInfoListTile(info: namedColor.toRGBString()),
-          _ColorInfoListTile(info: namedColor.toHSLString()),
-          _ColorInfoListTile(info: namedColor.toHSVString()),
-          _ColorInfoListTile(info: '${namedColor.color.computeLuminance().toStringAsFixed(4)} ($brightnessString)'),
-        ],
+      child: TextButtonTheme(
+        data: TextButtonThemeData(
+          style: TextButton.styleFrom(primary: contrastColor),
+        ),
+        child: ListView(
+          padding: const EdgeInsets.all(16.0),
+          shrinkWrap: true,
+          children: <Widget>[
+            _ColorInfoListTile(
+              info: namedColor.name,
+              subtitle: 'name',
+            ),
+            _ColorInfoListTile(info: namedColor.toHexTriplet(), subtitle: 'hex triplet'),
+            _ColorInfoListTile(info: namedColor.toRGBString(), subtitle: 'red, green, blue'),
+            _ColorInfoListTile(
+                info: namedColor.toHSLString(), subtitle: 'hue, saturation, lightness'),
+            _ColorInfoListTile(info: namedColor.toHSVString(), subtitle: 'hue, saturation, value'),
+            _ColorInfoListTile(
+              info: namedColor.color.computeLuminance().toStringAsFixed(4),
+              subtitle: 'relative luminance',
+            ),
+            _ColorInfoListTile(info: namedColor.isDark ? 'dark' : 'light', subtitle: 'brightness'),
+          ],
+        ),
       ),
     );
   }
 }
 
 class _ColorInfoListTile extends StatelessWidget {
-  const _ColorInfoListTile({Key key, this.info}) : super(key: key);
+  const _ColorInfoListTile({Key key, this.info, this.subtitle}) : super(key: key);
 
   final String info;
+  final String subtitle;
 
   @override
   Widget build(BuildContext context) {
     return ListTile(
       title: Text(info),
-      trailing: IconButton(
-        icon: const Icon(Icons.content_copy),
+      subtitle: Text(subtitle),
+      trailing: TextButton(
+        child: const Text('copy'),
         onPressed: () {},
       ),
+      // trailing: Wrap(
+      //   children: [
+      //     IconButton(
+      //       // iconSize: 20.0,
+      //       icon: const Icon(Icons.content_copy),
+      //       onPressed: () {},
+      //     ),
+      //     IconButton(
+      //       // iconSize: 20.0,
+      //       icon: const Icon(Icons.search),
+      //       onPressed: () {},
+      //     ),
+      //   ],
+      // ),
+
+      // trailing: IconButton(
+      //   icon: const Icon(Icons.content_copy),
+      //   onPressed: () {},
+      // ),
     );
   }
 }
